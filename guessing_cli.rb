ea2a5bee9320cbae69090d0a4cd1a4f9d1946d30
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-21 17:36:34 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,17 @@
-#
+
+def guessing_game 
+  user_response = gets.chomp
+  
+end
+
+
+
+
+
+def run_guessing_game
+  puts "Guess a number between 1 and 6"
+  user_response = gets.chomp
+  
+  if user_response == "exit"
+    puts "Goodbye!"
+end
