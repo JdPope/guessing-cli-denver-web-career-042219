3f28eef3b90267fbf4925c0e6509099b71841fb5
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-21 17:43:27 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -17,5 +17,12 @@ end
 
 def run_guessing_game
   puts "Guess a number between 1 and 6"
-  guessing_game
+   user_response = gets.chomp.to_i
+  computer_number = rand(1..7)
+  if user_response == "exit"
+    puts "Goodbye!"
+  elsif user_response == computer_number
+    puts "You guessed the correct number!"
+  elsif user_response != computer_number
+  puts "The computer guessed #{computer_number}."
 end
